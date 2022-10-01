2329d12805ac2fa5199dbe03c80ff310024c83b4
Luc Veronneau <luc.veronneau@vertechcon.net>
2022-09-30 21:21:47 -0400

    rework image segment block

--- src/CommunAxiomWeb/Extend/ImageSegmentBlock.cs
+++ src/CommunAxiomWeb/Extend/ImageSegmentBlock.cs
@@ -9,7 +9,16 @@ namespace CommunAxiomWeb.Extend
         [Piranha.Extend.Field(Description = "Set image to the right.")]
         public CheckBoxField SetImageRight {get;set;}
         public ImageField ImageField { get; set; }
+        public SelectField<eImageSize> ImageSize { get; set; }
         public TextField Title { get; set; }
         public HtmlField Body { get; set; }
+
+        public enum eImageSize
+        {
+            Large,
+            Medium,
+            Small,
+            xlarge
+        }
     }
 }
